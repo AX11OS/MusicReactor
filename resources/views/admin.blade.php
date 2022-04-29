65a029c532fa39ae61d9b20013ec1d6e87e1e615
Alejandro Vazquez <alejandrovazquez3069@gmail.com>
2022-04-28 19:29:46 -0500

    Routes user & admin

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout_1')
+@section('title','Admin')
+
+@section('content')
+    <h1>Pagina Admin</h1>
+    <div id="example"></div>
+@endsection
---
+++ resources/views/user.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout_1')
+@section('title','User')
+
+@section('content')
+    <h1>Pagina User</h1>
+    <div id="example"></div>
+@endsection
