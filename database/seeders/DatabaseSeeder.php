ac1a362d0a2a456d28a22af6083e14d3ed357728
Alejandro Vazquez <alejandrovazquez3069@gmail.com>
2022-05-26 17:19:11 -0500

    Migrations
    Migrations III + Seeders + timestamps update
    
    Fix invoices
    
    Fix genrers table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Album;
-use App\Models\Gender;
+use App\Models\Genrer;
 use App\Models\Musician;
 use App\Models\Song;
 use Illuminate\Database\Seeder;
@@ -17,10 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        Gender::factory(10)->create();
+
+        //Run seeder     php artisan db:seed
+
+
+         \App\Models\User::factory(60)->create();
+        /* Genrer::factory(10)->create();
         Musician::factory(50)->create();
         Album::factory(15)->create();
-        Song::factory(50)->create();
+        Song::factory(50)->create(); */
     }
 }
